72427e8bd83a82ce52073ee22f527b14b4dfa686
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2023-07-17 12:40:11 +0800

    page style

---
+++ src/components/SharePage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineEmits<{
+    (event: 'create'): void
+}>()
+</script>
+
+<template>
+    <section class="text-center">
+        <h2 class="font-light mb-4">
+            Create chatroom, share with it.
+        </h2>
+        <button class="cool-link" @click="$emit('create')">Create</button>
+        <div>
+
+        </div>
+    </section>
+</template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,29 +1,59 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import LandingPage from '@/components/LandingPage.vue';
+import SharePage from '@/components/SharePage.vue';
 
 const router = useRouter()
 
-const formState = ref({
-    key: ""
+const roomNumber = ref('')
+const createPage = ref(false)
+
+const activeComponent = computed(() => {
+    return createPage.value ? SharePage : LandingPage
 })
 
-function nextHandler(e: MouseEvent) {
-    e.preventDefault()
-    const roomid = formState.value.key
-    if (!roomid) return
-    router.replace({ name: 'chatroom', params: { roomid } })
+function createRoomNumber() {
+    roomNumber.value = '1234'
+}
+
+function nextHandler() {
+    if (createPage.value) {
+        const roomid = roomNumber.value
+        if (!roomid) return
+        router.replace({ name: 'chatroom', params: { roomid } })
+        return
+    }
+    createPage.value = true
 }
 </script>
 
 <template>
     <div class="md:container relative mx-auto w-full h-full grid place-items-center">
-        <LandingPage />
-        <button class="fixed-button font-light cool-link" style="--btn-theme: black" @click="nextHandler">
+        <Transition name="fade" mode="out-in">
+            <component :is="activeComponent" @create="createRoomNumber"></component>
+        </Transition>
+        <button class="fixed-bottom-right font-light disabled:opacity-50" :class="{ 'cool-link': !createPage }"
+            @click="nextHandler" :disabled="createPage && !roomNumber">
             Next
         </button>
 
     </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: transform 0.2s ease-out;
+}
+
+.fade-enter-from {
+    transform: translateX(100%);
+}
+
+.fade-leave-to {
+    /* opacity: 0; */
+    transform: translateX(-100%);
+}
+</style>
 
