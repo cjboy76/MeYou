e88797c27b0631bb3990889022cc9643eb91e0ea
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2023-07-18 15:10:29 +0800

    chatroom controllerbar

---
+++ src/components/ControllerBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { GuidanceNoNoise, GuidanceNoVideo, GuidanceVideo, GuidanceVolume, GuidancePhone } from "@/icons";
+
+const props = defineProps({
+    cameraOn: Boolean,
+    voiceOn: Boolean,
+})
+</script>
+
+<template>
+    <div class=" fixed bottom-[10lvh] left-1/2 -translate-x-1/2 w-full h-auto flex justify-center">
+        <button class="mx-2 p-4 rounded-full text-white bg-slate-900 bg-opacity-50">
+            <GuidanceVideo v-show="props.cameraOn" class=" w-8 h-8" />
+            <GuidanceNoVideo v-show="!props.cameraOn" class="w-8 h-8" />
+        </button>
+        <button class="mx-2 p-4 rounded-full text-white bg-slate-900 bg-opacity-50">
+            <GuidanceVolume v-show="props.voiceOn" class="w-8 h-8" />
+            <GuidanceNoNoise v-show="!props.voiceOn" class="w-8 h-8" />
+        </button>
+        <button class="mx-2 p-4 rounded-full text-white bg-slate-900 bg-opacity-50">
+            <GuidancePhone class="w-8 h-8" />
+        </button>
+    </div>
+</template>
---
+++ src/icons/index.ts
@@ -0,0 +1,9 @@
+import GuidanceNoNoise from './GuidanceNoNoise.vue'
+import GuidanceNoVideo from './GuidanceNoVideo.vue'
+import GuidanceVideo from './GuidanceVideo.vue'
+import GuidanceVolume from './GuidanceVolume.vue'
+import GuidancePhone from './GuidancePhone.vue'
+
+export {
+    GuidanceNoNoise, GuidanceNoVideo, GuidanceVideo, GuidanceVolume, GuidancePhone
+}
