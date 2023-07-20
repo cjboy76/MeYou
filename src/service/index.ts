a7a4e13afb84844c65f894ad9c665a556fca9c10
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2023-07-20 12:42:48 +0800

    firebase interfaces

---
+++ src/service/index.ts
@@ -0,0 +1,44 @@
+import { collection, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { db } from "./firebase";
+
+const collectionName = "activeRoom"
+
+export async function createRoom(hostId: string, onError?: (error: unknown) => void) {
+    try {
+        await addDoc(collection(db, collectionName), {
+            active: true,
+            hostId,
+        });
+
+    } catch (error) {
+        if (onError) {
+            onError(error)
+        }
+    }
+}
+
+export async function updateGuest(roomNumber: string, guestId: string, onError?: (error: unknown) => void) {
+    try {
+        const roomRef = doc(db, collectionName, roomNumber);
+
+        return await updateDoc(roomRef, {
+            guestId
+        });
+    } catch (error) {
+        if (onError) {
+            onError(error)
+        }
+    }
+}
+
+
+export async function destroyRoom(roomNumber: string, onError?: (error: unknown) => void) {
+    try {
+        await deleteDoc(doc(db, collectionName, roomNumber));
+    } catch (error) {
+        if (onError) {
+            onError(error)
+        }
+    }
+}
+
